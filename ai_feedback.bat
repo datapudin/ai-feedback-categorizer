1507c4fed578a5bb27687fccbd0140898250d284
datapudin <162911330+datapudin@users.noreply.github.com>
2025-06-29 02:43:51 +0530

    Add files via upload

---
+++ ai_feedback.bat
@@ -0,0 +1,5 @@
+@echo off
+call C:\Users\avish\anaconda3\Scripts\activate.bat
+call conda activate ai-feedback-env
+streamlit run app.py
+pause
---
+++ scraper.bat
@@ -0,0 +1,5 @@
+@echo off
+call C:\Users\avish\anaconda3\Scripts\activate.bat
+call conda activate ai-feedback-env
+python scraper.py
+pause
